c0303156d2fbe1d73721c443699a000012ba68d9
Andree-Anne Roy <andree-anne.roy2009@hotmail.com>
2018-11-27 21:33:19 -0500

    Add project files.

---
+++ Calculatrice/Controllers/CalculatriceController.cs
@@ -0,0 +1,52 @@
+﻿using Calculatrice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculatrice.Controllers
+{
+    public class CalculatriceController : Controller
+    {
+        // GET: Calculatrice
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(CalculatriceViewModel model, string command)
+        {
+            if (command == "addition")
+            {
+                model.Resultat = model.premierChiffre + model.deuxiemeChiffre;
+            }
+            if (command == "soustraction")
+            {
+                model.Resultat = model.premierChiffre - model.deuxiemeChiffre;
+            }
+            if (command == "multiplication")
+            {
+                model.Resultat = model.premierChiffre * model.deuxiemeChiffre;
+            }
+            if (command == "division")
+            {
+                model.Resultat = model.premierChiffre / model.deuxiemeChiffre;
+            }
+            if (command == "exposant")
+            {
+                model.Resultat = Math.Pow(model.premierChiffre, model.deuxiemeChiffre);
+            }
+            if (command == "moyenne")
+            {
+                model.Resultat = ((model.premierChiffre + model.deuxiemeChiffre) / 2);
+            }
+            if (command == "modulo")
+            {
+                model.Resultat = model.premierChiffre % model.deuxiemeChiffre;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Calculatrice/Models/CalculatriceViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculatrice.Models
+{
+    public class CalculatriceViewModel
+    {
+        public double premierChiffre
+        {
+            get;
+            set;
+        }
+        public double deuxiemeChiffre
+        {
+            get;
+            set;
+        }
+        public double Resultat
+        {
+            get;
+            set;
+        }
+    }
+}
